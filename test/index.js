8678e7900f07b164673c32c3bfda3361927d4939
Peter Braden <peterbraden@peterbraden.co.uk>
2013-02-12 08:52:59 -0800

    Add test for unicodeString

--- test/index.js
+++ test/index.js
@@ -1,13 +1,22 @@
 var cards = require('../lib/cards')
  , vows = require('vows')
+ , assert = require('assert')
 
 
  vows.describe("Card Deck").addBatch({
   "Poker Deck": {
-    topic: new cards.PokerDeck(),
+    topic: function(){return new cards.PokerDeck()},
+
+    draw : function(deck){
+      var card = deck.draw();
+      assert.equal(card.toString(), "[Card club:2]");
+      assert.equal(card.unicodeString(), "2♣");
+    },
+
     shuffleAll : function(deck){
       deck.shuffleAll()
     }
+
   }
 
 
