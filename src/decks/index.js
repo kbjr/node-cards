b0be8dd7aae77e8d4e4530443cbe4f612176d754
James Brumond <james@jbrumond.me>
2018-09-16 19:59:53 -0700

    some cleanup of exports

---
+++ src/decks/index.js
@@ -0,0 +1,18 @@
+
+const { StandardDeck } = require('./decks/standard');
+const { MajorArcanaDeck } = require('./decks/major-arcana');
+const { EuchreDeck } = require('./decks/euchre');
+const { PinochelDeck } = require('./decks/pinochel');
+const { BarajaDeck } = require('./decks/baraja');
+const { TarotDeck } = require('./decks/tarot');
+const { PiquetDeck } = require('./decks/piquet');
+
+module.exports = {
+	StandardDeck,
+	MajorArcanaDeck,
+	EuchreDeck,
+	PinochelDeck,
+	BarajaDeck,
+	TarotDeck,
+	PiquetDeck
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 const { useArc4, seedArc4 } = require('./rand');
 
-const { Card, FoolCard, JokerCard, TrumpCard, MajorArcanaCard } = require('./card');
+const { Card, JokerCard } = require('./card');
 const { Suit, spades, hearts, diamonds, clubs, trump, swords, cups, coins, wands, majorArcana, none } = require('./suits');
 const {
 	Rank,
@@ -13,12 +13,7 @@ const {
 } = require('./ranks');
 
 const { Deck } = require('./deck');
-const { StandardDeck } = require('./decks/standard');
-const { MajorArcanaDeck } = require('./decks/major-arcana');
-const { EuchreDeck } = require('./decks/euchre');
-const { PinochelDeck } = require('./decks/pinochel');
-const { BarajaDeck } = require('./decks/baraja');
-const { TarotDeck } = require('./decks/tarot');
+const { StandardDeck, MajorArcanaDeck, EuchreDeck, PinochelDeck, BarajaDeck, TarotDeck, PiquetDeck } = require('./decks');
 
 // Always make sure this is imported last, as it has to populate maps defined in the suits file
 const unicode = require('./unicode');
@@ -27,18 +22,12 @@ module.exports = {
 	useArc4,
 	seedArc4,
 	Card,
-	FoolCard,
 	JokerCard,
-	TrumpCard,
-	MajorArcanaCard,
 	Deck,
-	StandardDeck,
-	MajorArcanaDeck,
-	EuchreDeck,
-	PinochelDeck,
-	BarajaDeck,
-	TarotDeck,
-	unicode,
+	decks: {
+		StandardDeck, MajorArcanaDeck, EuchreDeck,
+		PinochelDeck, BarajaDeck, TarotDeck, PiquetDeck
+	},
 	Suit,
 	suits: {
 		spades, hearts, diamonds, clubs, trump,
@@ -52,5 +41,6 @@ module.exports = {
 		arcana12, arcana13, arcana14, arcana15, arcana16, arcana17, arcana18, arcana19, arcana20, arcana21,
 		trump0, trump1, trump2, trump3, trump4, trump5, trump6, trump7, trump8, trump9, trump10, trump11,
 		trump12, trump13, trump14, trump15, trump16, trump17, trump18, trump19, trump20, trump21
-	}
+	},
+	unicode
 };
