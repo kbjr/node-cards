edb630c8287776e18b0906e344f74a9a21e9f5da
ismemang <ismemang@slashn.io>
2022-07-15 15:15:34 -0700

    Formatting tools.

---
+++ .prettierrc.js
@@ -0,0 +1,34 @@
+/** Simple logger. */
+/* istanbul ignore next: Unnecessary. */
+if (!global.log) global.log = console;
+
+/** Formatter configuration. */
+const config = {
+	arrowParens: 'always',
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	printWidth: 80,
+	proseWrap: 'never',
+	quoteProps: 'as-needed',
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'all',
+	useTabs: true,
+	overrides: [
+		{
+			files: ['**/*.json', '**/*.yaml', '**/*.yml'],
+			options: {
+				useTabs: false,
+			},
+		},
+	],
+};
+
+if (typeof require !== 'undefined' && require.main === module) {
+	// When a script.
+	log.debug(JSON.stringify(config, null, 2));
+} else {
+	// When a module.
+	module.exports = config;
+}
